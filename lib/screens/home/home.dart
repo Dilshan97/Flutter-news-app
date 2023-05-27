545e0e368413c5751772dd394bc338f8a314e42b
Dilshan Ramesh <dilshanramesh81@gmail.com>
2023-05-27 16:00:10 +0530

    some screens added

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News App'),
+      ),
+      body: Container(
+    
+      ),
+    );
+  }
+}
